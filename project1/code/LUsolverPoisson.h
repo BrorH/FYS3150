6880158af530d01a6133ab8cffc548f07910f12b
haakooto <hakonolav.torvik@gmail.com>
2020-08-21 18:03:41 +0200

    fix dataflow, make less convoluted

--- project1/code/LUsolverPoisson.h
+++ project1/code/LUsolverPoisson.h
@@ -5,113 +5,133 @@
 using namespace std;
 using namespace arma;
 
-class LUsolverPoisson {
-  Mat<double> A, b, xn;
+class LUsolverPoisson
+{
+  mat A, b, xn, v;
   double h, x_min, x_max, u0, u1;
   int n;
   double (*f)(double);
 
 public:
-  LUsolverPoisson(double (*_f)(double), double _u0,
-                  double _u1) { // Mat<double> _A, Mat<double> _b) {
+  LUsolverPoisson(double (*_f)(double), mat _xn)
+  {
     f = _f;
-    u0 = _u0;
-    u1 = _u1;
-  }
-  void setup(double _h, double _n, Mat<double> _xn) {
-    h = _h;
-    n = _n;
     xn = _xn;
+    n = xn.n_rows;
+    x_min = xn[0];
+    x_max = xn[n - 1];
+
     // construct_A();
     // construct_b();
-    // if a vector of x_n values are provided, the yare chosen
+    // if a vector of x_n values are provided, they aare chosen
 
     // set the parameters of the system
   }
-  void setup(double _h, int _n, double _x_min, double _x_max) {
+  LUsolverPoisson(double (*_f)(double), int _n, double _x_min, double _x_max)
+  {
     // if a vector of x_n values are NOT provided, x is evenly distributed on
     // range
-    double delta = _x_max - _x_min;
-    Mat<double> _xn;
-    _xn.set_size(_n, 1);
-    for (int i = 0; i < _n; i++) {
-      _xn(i) = (delta / (double)_n) * (double)i;
+    f = _f;
+    n = _n;
+    h = (_x_max - _x_min) / (double)n;
+    xn.set_size(n, 1);
+    for (double i = 0; i < n; i++)
+    {
+      xn(i) = h * i;
     }
-    setup(_h, _n, _xn);
+  }
+  void set_boundary(double _u0, double _u1)
+  {
+    u0 = _u0;
+    u1 = _u1;
+    construct_A();
+    construct_b();
   }
 
-  Mat<double> compute(bool write = true) {
+  void compute(bool write = true)
+  {
     // runs the numerical integration and writes to the files v.dat and x.dat
-    Mat<double> L, U;
+    mat L, U;
     lu(L, U, A);
-    Mat<double> y = fwdsub(L, b);
-    Mat<double> v = bwdsub(U, y);
-    if (write) {
-      ofstream xfile("x.dat");
-      xfile << h << " " << n << endl;
-      xn.save(xfile, csv_ascii);
-      xfile.close();
-      ofstream vfile("v.dat");
-      vfile << h << " " << n << endl;
-      v.save(vfile, csv_ascii);
-      vfile.close();
-    }
-    return v;
+    mat y = fwdsub(L, b);
+    v = bwdsub(U, y);
+  }
+  void write(string filename)
+  {
+    ofstream xfile("x.dat");
+    xfile << h << " " << n << endl;
+    xn.save(xfile, csv_ascii);
+    xfile.close();
+    ofstream vfile("v.dat");
+    vfile << h << " " << n << endl;
+    v.save(vfile, csv_ascii);
+    vfile.close();
   }
 
 private:
-  void construct_A() {
+  void construct_A()
+  {
     // makes and fills the matrix A
     // note that it is very easy to convert this to a funciton that
     // allows for any values along, and next to, the diagonal!
     A.set_size(n, n);
     A.fill(0);
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
+    {
       // fill A with the diagonal elements
       A(i, i) = 2;
-      if (i != n - 1) {
+      if (i != n - 1)
+      {
         A(i + 1, i) = -1;
         A(i, i + 1) = -1;
       }
     }
   }
-  void construct_b() {
+  void construct_b()
+  {
     // makes and fills the array b
     b.set_size(n, 1);
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
+    {
       b(i) = pow(h, 2) * f(xn(i));
     }
   }
 
-  Mat<double> fwdsub(Mat<double> L, Mat<double> b) {
+  mat fwdsub(mat L, mat b)
+  {
     // performs forward subsititution on the lower triangular matrix L in the
     // sytem Lx=b
-    int size = (int)b.size();
-    Mat<double> x(size, 1);
+    int size = b.n_rows;
+    mat x(size, 1);
     x(0) = b(0) / L(0, 0);
     double sum;
-    for (int m = 1; m < size; m++) {
+    for (int i = 1; i < size; i++)
+    {
       sum = 0;
-      for (int i = 0; i < m; i++) {
-        sum += L(m, i) * x(i);
+      for (int j = 0; j < i; j++)
+      {
+        sum += L(i, j) * x(j);
       }
-      x(m) = (b(m) - sum) / L(m, m);
+      x(i) = (b(i) - sum) / L(i, i);
     }
     return x;
   }
-  Mat<double> bwdsub(Mat<double> U, Mat<double> b) {
+  mat bwdsub(mat U, mat b)
+  {
     // performs backwards subsititution on the upper triangular matrix L in the
     // sytem Ux=b
-    int size = (int)b.size();
-    Mat<double> x(size, 1);
+    int size = b.n_rows;
+    mat x(size, 1);
     x(size - 1) = b(size - 1) / U(size - 1, size - 1);
     double sum;
-    for (int m = size - 2; m >= 0; m--) {
+    for (int i = size - 2; i >= 0; i--)
+    {
       sum = 0;
-      for (int i = m; i < size; i++) {
-        sum += U(m, i) * x(i);
+      for (int j = i; j < size; j++)
+      {
+        sum += U(i, j) * x(j);
       }
-      x(m) = (b(m) - sum) / U(m, m);
+      x(i) = (b(i) - sum) / U(i, i);
     }
     return x;
   }
--- project1/code/main.cpp
+++ project1/code/main.cpp
@@ -1,14 +1,17 @@
 #include "LUsolverPoisson.h"
+#include <cmath>
 static double e = 2.718281828459045;
 
 double f(double x) { return 100 * pow(e, -10 * x); }
 
-int main() {
-  double h = 0.001;
-  int n = 1000;
+int main(int argc, char *argv[])
+{
+  int exp = atoi(argv[1]);
+  int n = pow(10, exp);
 
-  LUsolverPoisson solver(*f, 0, 0);
-  solver.setup(h, n, 0, 1);
-  // solver.compute();
+  LUsolverPoisson solver(*f, n, 0, 1);
+  solver.set_boundary(0, 0);
+  solver.compute();
+  solver.write("data.txt");
   return 0;
 }
