1dee12a2e794ae7d515d9fbeb1b4e20d2eefcbd8
Bror Hjemgaard <bahjemga@uio.no>
2020-08-23 23:27:52 +0200

    new double-array based method of solution. Need optimizing in LU-decomp. Ideas for optimizing noted in comments

--- project1/code/main.cpp
+++ project1/code/main.cpp
@@ -1,153 +1,141 @@
-// #include "LUsolverPoisson.h"
-// #include <cmath>
-#include "Matrix.h"
-#include "common.h"
+#include "time.h"
+#include <fstream>
+#include <iostream>
+#include <math.h>
 
+using namespace std;
 static double e = 2.718281828459045;
+
+void LUdcmp(double **A, int n, double **L, double **U) {
+  double sum = 0;
+  for (int i = 0; i < n; i++) {
+    for (int k = i; k < n; k++) {
+      for (int j = 0; j < i; j++) {
+
+        sum += L[i][j] * U[j][k];
+      }
+      U[i][k] = A[i][k] - sum;
+      sum = 0;
+    }
+    for (int k = i; k < n; k++) {
+      if (i == k) {
+        L[i][i] = 1;
+      }
+      for (int j = 0; j < i; j++) {
+
+        sum += L[k][j] * U[j][i];
+      }
+      L[k][i] = (A[k][i] - sum) / U[i][i];
+      sum = 0;
+    }
+  }
+}
+
+double *fwdsub(double **L, double *b, int n) {
+  double *y = new double[n];
+  y[0] = b[0] / L[0][0];
+  double sum = 0;
+  for (int i = 1; i < n; i++) {
+    for (int j = 0; j < i; j++) {
+      sum += L[i][j] * y[j];
+    }
+    y[i] = (b[i] - sum) / L[i][i];
+    sum = 0;
+  }
+  return y;
+}
+
+double *bwdsub(double **U, double *b, int n) {
+  double *v = new double[n];
+  v[n - 1] = b[n - 1] / U[n - 1][n - 1];
+  double sum = 0;
+  for (int i = n - 2; i >= 0; i--) {
+    for (int j = i; j < n; j++) {
+      sum += U[i][j] * v[j];
+    }
+    v[i] = (b[i] - sum) / U[i][i];
+    sum = 0;
+  }
+  return v;
+}
+
+void construct_A(double **A, int n) {
+  for (int i = 0; i < n; i++) {
+    A[i][i] = 2;
+    if (i != n - 1) {
+      A[i + 1][i] = -1;
+      A[i][i + 1] = -1;
+    }
+  }
+}
+
+void write(double *v, double *x, int n) {
+
+  ofstream file("num.dat");
+  file << n << endl;
+  for (int i = 0; i < n; i++) {
+    file << v[i] << " " << x[i] << endl;
+  }
+  file.close();
+}
+
+void print2d(double **A, int n) {
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n; j++) {
+      cout << A[i][j] << " ";
+    }
+    cout << endl;
+  }
+}
+void print1d(double *a, int n) {
+  for (int i = 0; i < n; i++) {
+    cout << a[i] << " ";
+  }
+  cout << endl;
+}
 
 double f(double x) { return 100 * pow(e, -10 * x); }
 
-#include <algorithm>
-//#include <armadillo>
-#include <fstream>
-#include <iostream>
-using namespace std;
-// using namespace arma;
+void printTime(clock_t start) {
 
-class LUsolverPoisson {
-  Matrix A, b, *xn, v;
-  double h, x_min, x_max, u0, u1;
-  int n;
-  double (*f)(double);
+  clock_t now = clock();
+  double elapsed = ((now - start) / (double)CLOCKS_PER_SEC);
+  cout << "elapsed time: " << elapsed << endl;
+}
 
-public:
-  LUsolverPoisson(double (*_f)(double), Matrix *_xn) {
-    f = _f;
-    xn = _xn;
-    n = xn->m;
-    x_min = xn->operator()(0, 0);
-    x_max = xn->operator()(n - 1, 0);
-    construct_A();
-    construct_b();
-    Matrix L, U;
-    lu(L, U, A);
-    Matrix y = fwdsub(L, b);
-    v = bwdsub(U, y);
-    // ofstream xfile("x.dat");
-    // xfile << h << " " << n << endl;
-    // xn.save(xfile, csv_ascii);
-    // xfile.close();
-    // ofstream vfile("v.dat");
-    // vfile << h << " " << n << endl;
-    // v.save(vfile, csv_ascii);
-    // vfile.close();
-    // construct_A();
-    // construct_b();
-    // if a vector of x_n values are provided, they aare chosen
-
-    // set the parameters of the system
+int main() {
+  // the process that takes the longest time is the LUdcmp
+  // how to make faster:
+  //  -Do not define L and U in outter scope (unnecesary mem)
+  //  -skip the matrix elements that are 0 anyway
+  clock_t start, finish;
+  start = clock();
+  int n = 10;
+  double h = 1 / (double)n;
+  double **A, **L, **U, *b, *x;
+  A = new double *[n];
+  L = new double *[n];
+  U = new double *[n];
+  b = new double[n];
+  x = new double[n];
+  for (int i = 0; i < n; i++) {
+    A[i] = new double[n];
+    L[i] = new double[n];
+    U[i] = new double[n];
+    x[i] = i * h;
+    b[i] = pow(h, 2) * f(x[i]);
   }
-  // LUsolverPoisson(double (*_f)(double), int _n, double _x_min, double _x_max)
-  // {
-  //   // if a vector of x_n values are NOT provided, x is evenly distributed on
-  //   // range
-  //   f = _f;
-  //   n = _n;
-  //   h = (_x_max - _x_min) / (double)n;
-  //   xn.set_size(n, 1);
-  //   for (double i = 0; i < n; i++) {
-  //     xn(i) = h * i;
-  //   }
-  // }
-  // void set_boundary(double _u0, double _u1) {
-  //   u0 = _u0;
-  //   u1 = _u1;
-  //
-  // }
+  printTime(start);
+  construct_A(A, n);
+  LUdcmp(A, n, L, U);
+  printTime(start);
 
-  // void compute(bool write = true) {
-  //   // runs the numerical integration and writes to the files v.dat and x.dat
-  //   Matrix L, U;
-  //   lu(L, U, A);
-  //   Matrix y = fwdsub(L, b);
-  //   v = bwdsub(U, y);
-  // }
-  // void write(string filename) {
-  //   ofstream xfile("x.dat");
-  //   xfile << h << " " << n << endl;
-  //   xn.save(xfile, csv_ascii);
-  //   xfile.close();
-  //   ofstream vfile("v.dat");
-  //   vfile << h << " " << n << endl;
-  //   v.save(vfile, csv_ascii);
-  //   vfile.close();
-  // }
+  double *y = fwdsub(L, b, n);
+  printTime(start);
 
-private:
-  void construct_A() {
-    // makes and fills the matrix A
-    // note that it is very easy to convert this to a funciton that
-    // allows for any values along, and next to, the diagonal!
-    A.set_size(n, n);
-    A.fill(0);
-    for (int i = 0; i < n; i++) {
-      // fill A with the diagonal elements
-      A(i, i) = 2;
-      if (i != n - 1) {
-        A(i + 1, i) = -1;
-        A(i, i + 1) = -1;
-      }
-    }
-  }
-  void construct_b() {
-    // makes and fills the array b
-    b.set_size(n, 1);
-    for (int i = 0; i < n; i++) {
-      b(i) = pow(h, 2) * f(xn(i));
-    }
-  }
+  double *v = bwdsub(U, y, n);
+  printTime(start);
+  write(v, x, n);
 
-  Matrix fwdsub(Matrix L, Matrix b) {
-    // performs forward subsititution on the lower triangular matrix L in the
-    // sytem Lx=b
-    int size = b.n_rows;
-    Matrix x(size, 1);
-    x(0) = b(0) / L(0, 0);
-    double sum;
-    for (int i = 1; i < size; i++) {
-      sum = 0;
-      for (int j = 0; j < i; j++) {
-        sum += L(i, j) * x(j);
-      }
-      x(i) = (b(i) - sum) / L(i, i);
-    }
-    return x;
-  }
-  Matrix bwdsub(Matrix U, Matrix b) {
-    // performs backwards subsititution on the upper triangular matrix L in the
-    // sytem Ux=b
-    int size = b.n_rows;
-    Matrix x(size, 1);
-    x(size - 1) = b(size - 1) / U(size - 1, size - 1);
-    double sum;
-    for (int i = size - 2; i >= 0; i--) {
-      sum = 0;
-      for (int j = i; j < size; j++) {
-        sum += U(i, j) * x(j);
-      }
-      x(i) = (b(i) - sum) / U(i, i);
-    }
-    return x;
-  }
-};
-
-int main(int argc, char *argv[]) {
-  int exp = atoi(argv[1]);
-  int n = pow(10, exp);
-
-  LUsolverPoisson solver(*f, n, 0, 1);
-  solver.set_boundary(0, 0);
-  solver.compute();
-  solver.write("data.txt");
   return 0;
 }
--- project1/code/plot.py
+++ project1/code/plot.py
@@ -1,22 +1,27 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def read(filename):
+    x = []
+    y = []
     with open(filename, "r+") as file:
         lines = file.read().split("\n")
-    conf = [float(i) for i in lines[0].split('\n')[0].split(' ')]
-    dat = [float(i) for i in lines[1:-1]]
-    return np.array(dat), conf
+    n = [float(i) for i in lines[0].split("\n")[0].split(" ")][0]
+    for line in lines[1:-1]:
+        foo = [float(a) for a in line.split(" ")]
+        y.append(foo[0])
+        x.append(foo[1])
+    return np.array(x), np.array(y), n
+    # dat = [float(i) for i in lines[1:-1]]
+    # return np.array(dat), conf
 
 
+f = lambda y: 1 - (1 - np.exp(-10)) * y - np.exp(-10 * y)
 
-f = lambda y: 1-(1-np.exp(-10))*y-np.exp(-10*y)
-
-x = read("x.dat")[0]
-v = read("v.dat")[0]
-#print(v)
-plt.plot(x,v, label='num')
-plt.plot(x,f(x), label='ana')
+x, y, n = read("num.dat")
+plt.plot(x, y, label="num")
+plt.plot(x, f(x), label="ana")
 plt.grid()
 plt.legend()
 plt.show()
