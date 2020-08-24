0136cfc87233503ef4b968f034aec6793c7e495d
haakooto <hakonolav.torvik@gmail.com>
2020-08-24 18:10:49 +0200

    update for new write

--- project1/code/plot.py
+++ project1/code/plot.py
@@ -6,16 +6,14 @@ def read(filename):
     with open(filename, "r+") as file:
         lines = file.readlines()
     n = int(lines[0].strip())
-
-    X = np.zeros((2, n - 1))
-    for i, line in enumerate(lines[1:-1]):
-        foo = [float(a) for a in line.split(" ")]
-        X[:, i] = foo
-    return *X, n
+    v = np.asarray([float(i) for i in lines[1].strip().split(",")[:-1]])
+    return v, n
 
 f = lambda y: 1 - (1 - np.exp(-10)) * y - np.exp(-10 * y)
 
-y, x, n = read("num.dat")
+y, n = read("num.dat")
+x = np.linspace(0, 1, n)
+assert(len(x) == len(y))
 print("n = %G" % n)
 plt.plot(x, y, label="num")
 plt.plot(x, f(x), label="ana")
