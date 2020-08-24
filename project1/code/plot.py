5f6619c561f52140855701395f52334d084c1d12
haakooto <hakonolav.torvik@gmail.com>
2020-08-24 14:53:23 +0200

    elegantize file read

--- project1/code/plot.py
+++ project1/code/plot.py
@@ -3,23 +3,19 @@ import matplotlib.pyplot as plt
 
 
 def read(filename):
-    x = []
-    y = []
     with open(filename, "r+") as file:
-        lines = file.read().split("\n")
-    n = [float(i) for i in lines[0].split("\n")[0].split(" ")][0]
-    for line in lines[1:-1]:
-        foo = [float(a) for a in line.split(" ")]
-        y.append(foo[0])
-        x.append(foo[1])
-    return np.array(x), np.array(y), n
-    # dat = [float(i) for i in lines[1:-1]]
-    # return np.array(dat), conf
+        lines = file.readlines()
+    n = int(lines[0].strip())
 
+    X = np.zeros((2, n - 1))
+    for i, line in enumerate(lines[1:-1]):
+        foo = [float(a) for a in line.split(" ")]
+        X[:, i] = foo
+    return *X, n
 
 f = lambda y: 1 - (1 - np.exp(-10)) * y - np.exp(-10 * y)
 
-x, y, n = read("num.dat")
+y, x, n = read("num.dat")
 print("n = %G" % n)
 plt.plot(x, y, label="num")
 plt.plot(x, f(x), label="ana")
