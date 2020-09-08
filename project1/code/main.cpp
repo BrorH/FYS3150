35d0516f0a775630e39b392bd4f21e7d774fcf20
Bror Hjemgaard <bahjemga@uio.no>
2020-09-08 19:26:19 +0200

    added detailed comments

--- project1/code/main.cpp
+++ project1/code/main.cpp
@@ -5,11 +5,22 @@
 #include <string>
 using namespace std;
 
-// arguments: n, method, (int)write_solution
+/*
+Program arguments:
+ - n (int): size of matrix (in log10)
+ - method ("original" or "optimized"): method of solution
+ - write_solution (boolean int): write all solution points to solutions.dat. Takes long time for high n
+
+ Written by 
+ - Bror Hjemgaard, bahjemga@uio.no 
+ - Håkon Olav Torvik, haakooto@uio.no
+ 
+ In most loops, the individual FLOPS are noted. Condition statements are omitted.
+*/
 
 int n;
-double h;
-static double e = 2.718281828459045;
+long double h;
+static long double e = 2.718281828459045;
 
 double f(double x) { return 100 * pow(e, -10 * x); } // 3 FLOPS
 
@@ -17,26 +28,12 @@ double f(double x) { return 100 * pow(e, -10 * x); } // 3 FLOPS
 
 void LUdcmp_optimized(double *L, double *U)
 {
-  // this is the optimized LU decomposition that accounts for the
-  // tri-diagonal properties of A.
-  // U[i][k] = (u_ik if k=i), (-1 if k=i+1), (0 else) [u_lk def. in book]
-  // L[i][k] = (1 if k=i), (l_ik if k=i-1), (0 else) [l_ik def. in book]
-  // this optiouble sum = 0;mization is just a de-foorlooping of the complete decomposition.
-  // To achieve the best optimizitaion the algorithm may have to be reworked
-  // completely from a mathematical standpoint.
+  /*
+  Optimized LU decomposition that accounts for the tri-diagonal properties of A.
+  The analytic expressions for L and U are in the paper. Efficiency O(n)
+  */
 
-  // Study of the patters yield (PROVE THIS!!):
-  // u_ik (where k=i) = (i+2)/(i+1) for i >= 0.
-  // l_ik (where k=i-1) = -i/(i+1) for i > 0.
-
-  // to ram optimize we need to do some tricks with the indexes. The conversions
-  // are as follows:
-  // L[i][i] -> L[0] = 1
-  // L[i][i-1] -> L[i+1]
-  // U[i][i] -> U[i+1]
-  // U[i][i+1] -> U[0] = -1
-
-  for (double i = 0; i < n; i++) // n operations: 6n FLOPS
+  for (double i = 0; i < n; i++) // n iterations: 6n FLOPS
   {
     U[(int)i + 1] = (i + 2) / (i + 1); // 3 FLOPS
     if (i > 0)
@@ -48,9 +45,15 @@ void LUdcmp_optimized(double *L, double *U)
 
 void LUdcmp(double **A, double **L, double **U)
 {
-  // this is general LU decomposition.
+  /*
+  General LU-decomposition for matrix A. 
+  This is methodically identical to the mathematical decomposition, 
+  and according to Wikipedia using this algorithm requires 2/3 n^3 FLOPS: O(n^3).
+  (https://en.wikipedia.org/wiki/LU_decomposition#Using_Gaussian_elimination)
+  */
+
   double sum = 0;
-  for (int i = 0; i < n; i++) // n^3 FLOPS
+  for (int i = 0; i < n; i++)
   {
     for (int k = i; k < n; k++)
     {
@@ -73,32 +76,32 @@ void LUdcmp(double **A, double **L, double **U)
       }
       L[k][i] = (A[k][i] - sum) / U[i][i];
       sum = 0;
-      // cout << L[k][i] << endl;
     }
   }
 }
 
-// =================================================1======= //
+// ======================================================== //
+
 double *fwdsub_optimized(double *L, double *b)
 {
-  // The optimized version of forward substitution
-  // due to the definition of L (see comment in LUdcmp) many elements of the
-  // sum in y will be 0. In fact only the L[i][i-1] and L[i][i]-terms will be
-  // non-zero. And since the sum goes from j € [0, i-1], only one term in the
-  // sum is nonzero, i.e y[i] = (b[i]-L[i][i-1]*y[i-1])/L[i][i]
-
-  // see LUdcmp for the index trickery regarding ram optimization
+  /*
+  Optimized forward substitution. Efficiency O(n)
+  */
 
   double *y = new double[n];
   y[0] = b[0];
-  for (int i = 1; i < n; i++) // n-1 iterations: 2n FLOPS
+  for (int i = 1; i < n; i++) // n-1 iterations: 2n-2 FLOPS
   {
-    y[i] = (b[i] - L[i] * y[i - 1]); //2 FLOPS
+    y[i] = b[i] - L[i] * y[i - 1]; //2 FLOPS
   }
   return y;
 }
 double *fwdsub(double **L, double *b)
 {
+  /*
+  General forward substitution. Efficiency O(n^2)
+  */
+
   double *y = new double[n];
   y[0] = b[0] / L[0][0]; // 1 FLOP
   double sum = 0;
@@ -119,17 +122,13 @@ double *fwdsub(double **L, double *b)
 
 double *bwdsub_optimized(double *U, double *b)
 {
-  // optimized version of bwdsub. Like in fdwsub some of the elements in the
-  // sum will be 0, as only U[i][i] and U[i][i+1] will be nonzero. Unlike in
-  // fwdsub both of these terms will be included in the sum, and must be
-  // accounted for. However U[i][i+1] = -1, so no need to actually look up its
-  // value when summing
-  //
-  // see LUdcmp for the index trickery regarding ram optimization
+  /*
+  Optimized backwards substitution. Efficiency O(n)
+  */
+
   double *v = new double[n];
-  v[n - 1] = b[n - 1] / U[n];
-  for (int i = n - 2; i >= 0; i--) // n-2 iterations
-  // whole loop: 2n-4 FLOPS
+  v[n - 1] = b[n - 1] / U[n];      // 1 FLOP
+  for (int i = n - 2; i >= 0; i--) // n-2 iterations: 2n - 4 FLOPS
   {
     v[i] = (b[i] + v[i + 1]) / U[i + 1]; // 2 FLOPS
   }
@@ -138,6 +137,10 @@ double *bwdsub_optimized(double *U, double *b)
 
 double *bwdsub(double **U, double *b)
 {
+  /*
+  General backwards substitution. Efficiency O(n^2)
+  */
+
   double *v = new double[n];
   v[n - 1] = b[n - 1] / U[n - 1][n - 1]; // 1 FLOP
   double sum = 0;
@@ -157,7 +160,7 @@ double *bwdsub(double **U, double *b)
 
 void construct_A(double **A)
 {
-  // this is the same for non-optimized and cpu-optimized
+  // Fills A with its matrix elements
   for (int i = 0; i < n; i++)
   {
     A[i][i] = 2;
@@ -171,49 +174,47 @@ void construct_A(double **A)
 
 double max_rel_err(double *x, double *v)
 {
-  double exact;
-  double epsilon;
-  double max = 0;
+  /*
+  Computes the max relative error
+  */
+  double exact, epsilon, max;
+  max = 0;
   for (int i = 1; i < n - 1; i++)
   {
-    exact = 1 - (1 - (double)pow(e, -10)) * x[i] - (double)pow(e, -10 * x[i]);
-    exact = exact / pow(h, 2);
-    epsilon = log10(abs((v[i] - exact) / exact));
-    epsilon = abs(epsilon);
+    exact = (1 - (1 - (double)pow(e, -10)) * x[i] - (double)pow(e, -10 * x[i])) / pow(h, 2);
+    epsilon = abs((v[i] - exact) / exact);
     if (epsilon > max)
       max = epsilon;
   }
-  // cout << "max err = " << max << endl;
-  return max;
+  return abs(log10(max));
 }
 
 // ======================================================== //
 
 void write_solution(double *v)
 {
-  // writese just the solution points to the solutions.dat file
+  // Writes the contents of v to solutions.dat. Takes some time for large n
   ofstream file("solution.dat");
   for (int i = 0; i < n; i++)
   {
     file << v[i] << ",";
   }
-
   file.close();
 }
 
 void write_specs(float time, double max_err)
 {
-  // writes specifics about the simulations, n, time and max_err
+  // Writes n, elapsed time and max_err to specs.dat
   ofstream file("specs.dat");
   file << n << "," << time << "," << max_err << endl;
   file.close();
 }
 
-float printTime(clock_t start, string msg)
+float getTime(clock_t start)
 {
+  // returns the passed time from given start event
   clock_t now = clock();
   double elapsed = ((now - start) / (double)CLOCKS_PER_SEC);
-  // cout << msg << ": elapsed time: " << elapsed << endl;
   return elapsed;
 }
 
@@ -221,6 +222,9 @@ float printTime(clock_t start, string msg)
 
 void solve_original(bool write_sol = false)
 {
+  /*
+  Solves the system with the general, non-optimized method
+  */
   double **A, **L, **U, *b, *x;
   A = new double *[n];
   L = new double *[n];
@@ -235,13 +239,13 @@ void solve_original(bool write_sol = false)
     x[i] = i * h;
     b[i] = pow(h, 2) * f(x[i]);
   }
-  construct_A(A);
+  construct_A(A); // Fill A
 
-  clock_t start = clock();
-  LUdcmp(A, L, U);
+  clock_t start = clock(); // start timer
+  LUdcmp(A, L, U);         // Solve general LU decomposition
   double *y = fwdsub(L, b);
   double *v = bwdsub(U, y);
-  float done = printTime(start, "done");
+  float done = getTime(start);
 
   write_specs(done, max_rel_err(x, v));
   if (write_sol)
@@ -254,6 +258,7 @@ void solve_original(bool write_sol = false)
     delete[] L[i];
     delete[] U[i];
   }
+  // To avoid memory leak
   delete[] A;
   delete[] L;
   delete[] U;
@@ -265,38 +270,38 @@ void solve_original(bool write_sol = false)
 
 void solve_optimized(bool write_sol = false)
 {
+  /*
+  Solves the system for the special diagonal matrix with an optimized algorithm.
+  */
   double *L, *U, *b, *x;
   L = new double[n];
-  L[0] = 1;
+  L[0] = 1; // The first element of L is its constant diagonal element 1
 
   // fill U
   U = new double[n + 1];
-  U[0] = -1;
+  U[0] = -1; // The first element of U is its constant upper diagonal element -1
 
   // fill b and x
   x = new double[n];
   b = new double[n];
   for (int i = 0; i < n; i++)
-
   {
     x[i] = i * h;
     b[i] = pow(h, 2) * f(x[i]);
   }
 
-  LUdcmp_optimized(L, U);
-  clock_t start = clock();
+  LUdcmp_optimized(L, U);  // Optimized LU-decomposition
+  clock_t start = clock(); // Start time at start of numerical computation
   double *y = fwdsub_optimized(L, b);
   double *v = bwdsub_optimized(U, y);
-  float done = printTime(start, "done");
+  float done = getTime(start);
 
   double err = max_rel_err(x, v);
-  // cout << "error: " << err << endl;
   write_specs(done, err);
   if (write_sol)
   {
     write_solution(v);
   }
-
   delete[] L;
   delete[] U;
   delete[] b;
@@ -307,14 +312,12 @@ void solve_optimized(bool write_sol = false)
 
 int main(int argc, char *argv[])
 {
+  n = pow(10, atoi(argv[1])); // Read n from command line
+  h = 1 / (long double)n;
+  string method = argv[2]; // Solve either optimized or general
+  cout << "Solving " << method << " for log(n)= " << argv[1] << endl;
 
-  n = pow(10, atoi(argv[1]));
-  // n = 10;
-  h = 1 / (double)n;
-  string method = argv[2];
-  cout << "log10(n): " << argv[1] << endl;
-
-  bool write_sol = atoi(argv[3]); // if true  solved data points will be written
+  bool write_sol = atoi(argv[3]); // if true solved data points will be written to file
 
   if (method == "original")
   {
