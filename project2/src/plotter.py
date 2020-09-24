992fc05b7b46c050bea85a46c99e1245fbf1a345
haakooto <hakonolav.torvik@gmail.com>
2020-09-24 15:04:31 +0200

    nicer plots, plot error eigenvalues for beam

--- project2/src/plotter.py
+++ project2/src/plotter.py
@@ -1,7 +1,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib
 from datareader import read_data
 from bunch import Bunch
+import os
+import subprocess
+
+
+matplotlib.use(plt.get_backend())
+font = {"family": "DejaVu Sans", "weight": "bold", "size": 22}
+plt.rc("font", **font)
 
 
 class Plotter:
@@ -10,7 +18,7 @@ class Plotter:
         self.data = read_data("data.dat")
         self.rho = lambda n: np.linspace(0, self.kwargs.rho_max, n + 1, endpoint=False)[1:]
 
-        self.args = ("vec", "vecs", "counts")
+        self.args = ("vec", "vecs", "counts", "error")
 
         todo = self.kwargs.plot
         if todo is True:
@@ -23,9 +31,26 @@ class Plotter:
                 assert do in self.args
                 eval(f"self.plot_{do}()")
 
-    def show(self):
-        plt.legend()
-        plt.show()
+    def show(self, stump="tmp", xlab="x", ylab="y", size=(16, 11)):
+        plt.xlabel(xlab)
+        plt.ylabel(ylab)
+        plt.grid()
+        # plt.legend()
+
+        mng = plt.get_current_fig_manager()
+        mng.resize(*mng.window.maxsize())
+        fig = plt.gcf()
+        fig.set_size_inches(size, forward=False)
+
+        name = f"../figures/{stump}.png"
+        if self.kwargs.savefigs:
+            fig.savefig(name)
+        if self.kwargs.push:
+            pushFile(name)
+        if self.kwargs.noshow:
+            plt.clf()
+        else:
+            plt.show()
 
     def plot_vec(self):
 
@@ -33,13 +58,13 @@ class Plotter:
         for i, run in enumerate(self.kwargs.names):
             dat = self.data[run]
             idx = np.argsort(dat.eigvals)
-            idx = idx[1]
+            idx = idx[0]
             val = dat.eigvals[idx]
             vec = dat.eigvecs[:, idx]
 
             plt.plot(
                 self.rho(dat.n),
-                vec,
+                vec,# * val,
                 color=colors[i],
                 label=fr"n = {dat.n}, $\lambda$ = {val}",
             )
@@ -53,7 +78,7 @@ class Plotter:
         # hh = (N / rho_max) ** 2
         # d = 2 * hh
         # a = -hh
-        eigval = d + 2 * a * np.cos(np.pi / N)
+        # eigval = d + 2 * a * np.cos(np.pi / N)
         eigvec = np.asarray([np.sin(i * np.pi / N) for i in range(1, N)])
         eigvec /= np.linalg.norm(eigvec)
 
@@ -83,3 +108,38 @@ class Plotter:
         plt.plot(ns, transforms, "ro")
         plt.plot(x, a * x ** 2 + b * x + c, "k--", label=f"2nd degree polynomial fit")
         self.show()
+
+    def plot_error(self):
+        err = []
+        n = []
+        for name in self.kwargs.names:
+            dat = self.data[name]
+            N = dat.n + 1
+            h = dat.pmax / N
+            hh = h ** -2
+            avals = [2 * hh * (1 - np.cos(j * np.pi / N)) for j in range(1, N)]
+
+            val = dat.eigvals
+
+            err.append(max(abs(val - avals) / avals))
+            n.append(h)
+
+        h = np.log10(n)
+        err = np.log10(err)
+
+        x = np.linspace(min(h), max(h), len(h))
+        a, b = np.polyfit(h, err, deg=1)
+
+        plt.plot(x, x * a + b, "k--", label=f"fit. a = {a}")
+        plt.plot(h, err, "ro", label="max err")
+        plt.xlabel("log10(h)")
+        plt.ylabel("log10(err)")
+        plt.legend()
+        plt.show()
+
+
+def pushFile(filename):
+    path = os.path.abspath(filename)
+    subprocess.run(f"git add {path}".split())
+    subprocess.run(f'git commit -m "automatic_figure_update" --quiet'.split())
+    subprocess.run(f"git push --quiet".split())
