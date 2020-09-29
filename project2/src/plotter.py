0e9ee6bdb4f211ad54b14f59fba0c180072d3b01
Bror Hjemgaard <bahjemga@uio.no>
2020-09-29 18:53:23 +0200

    added doc

--- project2/src/plotter.py
+++ project2/src/plotter.py
@@ -8,6 +8,46 @@ from argHandler import argHandler
 from colour import Color
 from master import run
 from scipy import optimize
+mpl.use(plt.get_backend())
+plotHelp = """
+
+plotter.py, main plotting file.
+
+each function below plots a figure. 
+The functions have default arguments (set in the function definition), and running the functions without any arguments will produce figures similar to those in the paper.'
+In order to pass custom arguments, use same command line notation as with master.py after firstly passing the function name
+i.e the function "transforms" below may be called as
+ $ python3 plotter.py transforms // produces standard graph (as long as the file "data.dat" contains the right data to be plotted)
+ $ python3 plotter.py transforms n=1:100:5 eps=20 // n is in range(1,100,5) and eps = 5
+ $ python3 plotter.py transforms n=1:100:5 datafile=transdata.dat // same n range as above, but this time the data to be plotted is retreived from data/transdata.dat
+ $ python3 plotter.py transforms eps=50 sim=True datafile=transform.dat // eps=50, and in order to plot the system it runs a simulation (sim=True) which is stored into and read from data/transform.dat
+
+The calling of functions of custom parameters comes from a custom class in "argHandler.py" made specifically for this project.
+All (some exeptions) parameters in the function definitions below can be called like this from the commandline.
+Some parameters are common between (allmost) all functions:
+    -n: matrix size, integration points (N-1)
+    -rhomax: upper value of rho
+    -eps: epsilon, tolerance
+    -method: 0, 1 or 2. 0 for buckling beam problem, 1 for single-electron, and 2 for double-electron
+    -datafile: Specify which file the data to be plotted is located in (must be in subdir data/)
+    -sim: bool. if a dataset has not yet been generated/simulated setting sim=True will run the simulation for teh sepcified vales of n,rhomax,eps,method and omega. 
+
+generally you should not change the "method" parameter as each graphing function is custom to a method.
+Any extraordinary parameters (different from the 6 mentioned above) is noted below each function.
+
+the different functions are:
+- transforms: Plots the counted transforms of a simulated system as a function of n.
+
+- optimalRhomaxSingleElectron: Plots eigval rel. error as a function of rhomax. Prints lowest relative error and corresponding rhomax
+    
+- eigvalsSingleElectron: Plots the numerical eigenvalues of the single electron system against either n or rhomax.
+    special parameters: 
+        -n, rhomax: only ONE of these must be a list of values to be plotted against.
+        -eigcount: how many eigvals are to ble plotted
+- eigvalAccuracySingleElectron: Plots eigval accuracy against n and attemps a function fit of shape a*log10(n)+b
+    special parameters:
+        - fitIdxStart: The index of n where the function fit begins
+"""
 
 """
 MAIN PROGRAM FOR PLOTTING AND CREATING THE FIGURES IN THE PAPER
@@ -24,28 +64,28 @@ options:
 """
 
 
-def plotTransforms(n=np.arange(1.10), sim=False, datafile="data.dat", eps = 12, rhomax = [1,10,20], omega=[0,1,5],**kwargs):
+def transforms(n=range(1,10), rhomax = [1,10,20], eps = 12, omega = [0,1,5], method = [0,1,2], sim=False, datafile="data.dat"):
     # plots num of transformations against N on axis ax, as well as comparing to n**2 and n**2*ln(n)
     if sim:
         print("Solving ...")
         
         open(f"data/{datafile}", "w+").close() # clear file
-        for method in [0,1,2]:
+        for method_ in method:
             start = time.time()
             for N in n:
-                subprocess.run(f"./main.out {N}{eps}{rhomax[method]}{method}{omega[method]} {N} {eps} {rhomax[method]} {method} {omega[method]} {datafile}".split())
-                print(f"{round(100*(N-n[0])/(n[-1]-n[0]), 2)} %, N: {N}, eps: {eps}, method: {method}, rhomax: {rhomax[method]}, omega: {omega[method]}")
+                subprocess.run(f"./main.out {N}{eps}{rhomax[method_]}{method_}{omega[method_]} {N} {eps} {rhomax[method_]} {method_} {omega[method_]} {datafile}".split())
+                print(f"{round(100*(N-n[0])/(n[-1]-n[0]), 2)} %, N: {N}, eps: {eps}, method: {method_}, rhomax: {rhomax[method_]}, omega: {omega[method_]}")
             print(f"Done in {round(time.time() -start,3)} s.")
         #if datafile != "data.dat":
             # move content to designated file
             #subprocess.run(f"cp data.dat {datafile}".split())
     
     sols = read_data(datafile) #all solutions
-    for method in [0,1,2]: # all methods are plotted
-        trans = np.array([sols[f"{N}{eps}{rhomax[method]}{method}{omega[method]}"].transformations for N in n]) # array of counted transformations
+    for method_ in method: # all methods are plotted
+        trans = np.array([sols[f"{N}{eps}{rhomax[method_]}{method_}{omega[method_]}"].transformations for N in n]) # array of counted transformations
         
-        color = {0:"red", 1:"blue", 2:"green"}[method] #every method gets unique color
-        label = {0:"Buckling beam", 1:r"Quantum 1, $\rho_{max} = $"+str(rhomax[method]), 2:r"Quantum 2, $\rho_{max} = $"+f"{rhomax[method]}, $\omega_r = {omega[method]}$"}[method]
+        color = {0:"red", 1:"blue", 2:"green"}[method_] #every method gets unique color
+        label = {0:"Buckling beam", 1:r"Quantum 1, $\rho_{max} = $"+str(rhomax[method_]), 2:r"Quantum 2, $\rho_{max} = $"+f"{rhomax[method_]}, $\omega_r = {omega[method_]}$"}[method_]
 
         ax.plot(n, trans, "-o", markersize = 2.5, color=color, alpha=0.8, lw=2.8, label=label)
         
@@ -58,10 +98,10 @@ def plotTransforms(n=np.arange(1.10), sim=False, datafile="data.dat", eps = 12, 
         ax.set_ylabel("Transformations, $m$")
         plt.title(r"Transformations; $m$" )
 
-        print(f" eps: {eps}, method: {method}, rhomax: {rhomax[method]}, omega: {omega[method]}:")
+        print(f" eps: {eps}, method: {method_}, rhomax: {rhomax[method_]}, omega: {omega[method_]}:")
 
 
-def plotOptimalRhomaxQuantum1(rhomax = np.linspace(3.2,5.7, 1000), omega=12, method=1, n = 100, sim = False, datafile = "data.dat", eps = 12 ):
+def optimalRhomaxSingleElectron(n= 100,rhomax = np.linspace(3.2,5.7, 1000), eps = 12, omega=1, method=1, sim = False, datafile = "data.dat" ):
 
     # given a dataset with rhomax (rhomax) finds which rhomax makes each of the first 4 eigvals of quantum 1 the most accurate
     # produces plot as well
@@ -71,7 +111,7 @@ def plotOptimalRhomaxQuantum1(rhomax = np.linspace(3.2,5.7, 1000), omega=12, met
         open(f"data/{datafile}", "w").close() # clear file
         start = time.time()
         for _rhomax in rhomax:
-            subprocess.run(f"./main.out {n}{_rhomax} {n} {eps} {_rhomax} 1 1 {datafile}".split())
+            subprocess.run(f"./main.out {n}{_rhomax} {n} {eps} {_rhomax} {method} {omega} {datafile}".split())
             print(f"ρ: {round(_rhomax,3)}/{rhomax[-1]}, {round(100*(_rhomax-rhomax[0])/(rhomax[-1]-rhomax[0]), 2)} %")
         print(f"Done in {round(time.time()- start,3)} s")
         
@@ -105,38 +145,29 @@ def plotOptimalRhomaxQuantum1(rhomax = np.linspace(3.2,5.7, 1000), omega=12, met
     ax.set_ylabel(r"$\epsilon_{rel}$")
 
 
-def plotEigvalsQuantum1(n = 100, rhomax=np.linspace(4,6, 100), eigcount = 10, sim = False, datafile = "data.dat", eps = 16, **kwargs):
-    """
-    In the paper we wish to plot the eigenvalues both as a function of varying n and varying rhomax. 
-    The variable that varies is passed under "var" and the actually values of the variable under "vars". var can be "n" or "rhomax"
-    the static variable (opposite of the varying) must be passed as a kwarg, i.e 'var = "n", vars = range(1,10), rhomax=20'
-    eigcount is how many eigenvalues do you want to plot.
-    ax is the axis to be plotted on
-    """
-
-    # one of rhomax and n has to be 1-dimensional, as it is the constant
-    if not isinstance(n, list):
+def eigvalsSingleElectron(n = 100, rhomax = np.linspace(4,6, 100), eps = 16, omega = 1, method=1, eigcount = 10, sim = False, datafile = "data.dat"):
+    if not isinstance(n, list) and not isinstance(n, np.ndarray):
         var = "rhomax" #rhomax varies
         vars = rhomax
+        static = f"$n = {n}$"
     else:
         var = "n" #else n varies
         vars = n
+        static = r"$\rho_{max}=$" + f"${rhomax}$"
     if sim: # run simulation if prompted
         print("Solving ...")
         open(f"data/{datafile}", "w").close() # clear file
         start = time.time()
         if var == "n":
             for n in vars:
-                subprocess.run(f"./main.out {n}{rhomax} {n} {eps} {rhomax} 1 1 {datafile}".split())
+                subprocess.run(f"./main.out {n}{rhomax} {n} {eps} {rhomax} {method} {omega} {datafile}".split())
                 print(f"N: {n}/{vars[-1]}, {round(100*(n-vars[0])/(vars[-1]-vars[0]), 2)} %")
             print(f"Done in {round(time.time()- start,3)} s")
-            #subprocess.run(f"cp data.dat {datafile}".split())
         elif var == "rhomax":
             for rhomax in vars:
                 subprocess.run(f"./main.out {n}{rhomax} {n} {eps} {rhomax} 1 1 {datafile}".split())
                 print(f"ρ: {round(rhomax,3)}/{vars[-1]}, {round(100*(rhomax-vars[0])/(vars[-1]-vars[0]), 2)} %")
             print(f"Done in {round(time.time()- start,3)} s")
-            #subprocess.run(f"cp data.dat {datafile}".split())
     
     sols = read_data(datafile)
 
@@ -154,7 +185,7 @@ def plotEigvalsQuantum1(n = 100, rhomax=np.linspace(4,6, 100), eigcount = 10, si
         label = f"Matrix size, $n$"
     elif var == "rhomax":
         for i,rhomax in enumerate(vars):
-            ax.plot(indexes, np.sort(sols[f"{n}{rhomax}"].eigvals)[:eigcount],'-o', markersize=1, color=colors[i], lw=1.5, alpha=0.6, label="aaa")
+            ax.plot(indexes, np.sort(sols[f"{n}{rhomax}"].eigvals)[:eigcount],'-o', markersize=1, color=colors[i], lw=1.5, alpha=0.6)
         label = r"$\rho_{max}$"
     
     ax.plot(indexes, analytical, label="Analytical", color="black", lw=2, alpha=0.8, linestyle="--")
@@ -169,10 +200,10 @@ def plotEigvalsQuantum1(n = 100, rhomax=np.linspace(4,6, 100), eigcount = 10, si
     ax.figure.colorbar(mpl.cm.ScalarMappable(norm=norm, cmap=cmap), ax=ax, orientation='horizontal', label=label, ticks=[int(a) for a in np.linspace(vars[0], vars[-1],5)] )
     legend = ax.legend(fancybox=True, framealpha=1, shadow=True, borderpad=0.4, frameon = True,loc='lower right')
     legend.get_frame().set_facecolor('white')
+    ax.set_title(f"Eigenvalues for {static}")
 
 
-def plotEigvalAccuracyDynamicN(n = list(np.arange(10, 350)), eps = 12, rhomax=20, datafile="data.dat", sim=False, fitIdxStart = 10):
-    # WHAT IS fitIdxStart
+def eigvalAccuracySingleElectron(n = list(range(10, 350)), rhomax=20, eps = 12, method=1, fitIdxStart = 10, sim = False, datafile="data.dat"):
     if sim:
         open(f"data/{datafile}", "w+").close()
         for n_ in n:
@@ -205,31 +236,38 @@ def plotEigvalAccuracyDynamicN(n = list(np.arange(10, 350)), eps = 12, rhomax=20
     ax.set_title(f"Number of correct digits in eigenvalues")
 
 
-functionDesc = "To be filled"
+
 
 
 if __name__ == "__main__":
     with plt.style.context("seaborn-darkgrid"):
         f, ax = plt.subplots(1,1, dpi=100,frameon=True)
-        defaults = {"datafile":"data.dat"}#"sim":False, "eps":12, "n":50, "datafile":"data.dat", "rhomax":12, "omega":0, "method":0, "vars":[], "eigcount":10}
-        plotType = sys.argv[1]
-        assert plotType in dir(), f"{plotType} not found as function. Available types are:\n {functionDesc}"
+        defaults = {"datafile":"data.dat"}
         
-        flocals = inspect.getfullargspec(eval(plotType))
-        for var,val in zip(flocals.args, flocals.defaults):
-            if isinstance(val, np.ndarray):
-                val = [i for i in val ]
-            defaults[var] = val
+        if "help" in sys.argv:
+            print(plotHelp)
+            sys.exit()
         
-        _args = argHandler(defaults)
-    
-        args = _args.parse(sys.argv[1:])
+        try:
+            plotType = sys.argv[1]
+            assert plotType in dir(), f"{plotType} not found as function. Call program with 'help' for help"
+            
+            flocals = inspect.getfullargspec(eval(plotType))
+            for var,val in zip(flocals.args, flocals.defaults):
+                if isinstance(val, np.ndarray):
+                    val = [i for i in val ]
+                defaults[var] = val
         
-        args["datafile"] = "'"+str(args["datafile"])+"'"
-        call_args = ",".join([f'{arg}={args[arg]}' for arg in args.keys()])
-
+            args = argHandler(defaults).parse(sys.argv[1:])
+            
+            args["datafile"] = "'"+str(args["datafile"])+"'"
+            call_args = ",".join([f'{arg}={args[arg]}' for arg in args.keys()])
+        except Exception as e:
+            print(e)
+            print("plotter.py failed in initalization stage. passs arg 'help' for help and examples")
+            sys.exit()
         exec(f"{plotType}({call_args})")
-
+      
 
 plt.show()
 
