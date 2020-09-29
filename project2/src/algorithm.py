7ba4d00fd21d828692852fb8d5c23dac22d80b6f
haakooto <hakonolav.torvik@gmail.com>
2020-09-29 18:06:01 +0200

    remove old code

--- project2/src/algorithm.py
+++ project2/src/algorithm.py
@@ -16,6 +16,7 @@ def Jacobi(M, tol, V):
     # return M, V
     # else:
     # while emax > tol:
+    te = 0
     while True:
         Mprime = (M - np.diag(np.diag(M))) ** 2
         idx = np.argmax(abs(Mprime))
@@ -23,6 +24,7 @@ def Jacobi(M, tol, V):
         k = idx % n
         emax = Mprime[i, k]
         if emax < tol:
+            print(M)
             return np.diag(M), V
         tau = (M[i, i] - M[k, k]) / 2 / M[i, k]
         t1 = np.sqrt(1 + tau ** 2) - tau
@@ -40,36 +42,62 @@ def makeS(i, k, t, n):
     s = t * c
     S[i, i] = c
     S[k, k] = c
-    S[i, k] = s
-    S[k, i] = -s
+    S[i, k] = -s
+    S[k, i] = s
     return S
 
 
 def main():
-    n = 5
+    n = 50
     pmax = 1
     h = pmax / n
     hhneg = h ** -2
 
     A = np.zeros((n, n))
+    d = 2 * hhneg
+    a = -hhneg
     for i in range(n):
         if i != 0:
-            A[i, i - 1] = -hhneg
+            A[i, i - 1] = a
         if i != n - 1:
-            A[i, i + 1] = -hhneg
-        A[i, i] = 2 * hhneg
-    e = 1e-8
+            A[i, i + 1] = a
+        A[i, i] = d
+    e = 1e-4
 
-    D, V = Jacobi(A, e, np.identity(n))
-    print("Made it thourgh!")
+    # D, V = Jacobi(A, e, np.identity(n))
+    # print(V)
+    # print(D)
+    np.set_printoptions(precision=3)
     val, vec = np.linalg.eig(A)
-    print(val)
-    print(np.diag(D))
+    val = sorted(val)
+    for i in range(n):
+        print(val[i])
+    # for i in range(n):
+    #     for k in range(n):
+    #         print(f"{str(round(vec[i, k] ,4))}", end=" ")
+    #     print()
+    # print("Made it thourgh!")
+    sys.exit()
+    analytic_formula = [d + 2 * a * np.cos((i + 1) * np.pi / n) for i in range(n)]
+    vector_formula = np.zeros((n, n))
+    for j in range(1, n + 1):
+        vector_formula[j - 1] = [np.sin(k * j * np.pi / n) for k in range(1, n + 1)]
+    print(analytic_formula)
+    print(vector_formula)
     # S(4, 2, np.pi / 6, n)
-    print()
-    print(
-        (A @ V[:, 0]) / np.linalg.norm(A @ V[:, 0]), V[:, 0] / np.linalg.norm(V[:, 0])
-    )
+    for i in range(n):
+        print(vec[:, 0] @ vector_formula[i])
+
+    # print()
+    # print(
+    # (A @ V[:, 0]) / np.linalg.norm(A @ V[:, 0]), V[:, 0] / np.linalg.norm(V[:, 0])
+    # )
+    # c = ["r", "b", "g", "k", "c"]
+    # for i in range(n):
+    #     plt.plot(V[i], f"{c[i]}--", label=rf"$\lambda_{i}$")
+    #     plt.plot(vec[i], c[i])
+    # plt.legend()
+    # plt.show()
 
 
 if __name__ == "__main__":
